aa1d985163a420bd78a2ecb789ce1acfd5d21663
Syahreza Satria Alfath <goodypie013@gmail.com>
2025-10-08 22:11:51 +0700

    first commit

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+<section class="bg-white w-full p-4 mb-8">
+    <div class="flex justify-between max-w-4xl mx-auto items-center">
+        <a href="#" class=" font-bold text-2xl">Balance<span class="text-sky-500">Ku</span></a>
+        <nav class="flex items-center gap-8 text-sm text-neutral-500">
+            <a href="#" class="hover:text-sky-500 transition duration-300">Beranda</a>
+            <a href="#" class="hover:text-sky-500 transition duration-300">Hutang</a>
+            <a href="#" class="hover:text-sky-500 transition duration-300">Tabungan</a>
+            <a href="#" class="hover:text-sky-500 transition duration-300">Akun</a>
+        </nav>
+    </div>
+</section>
